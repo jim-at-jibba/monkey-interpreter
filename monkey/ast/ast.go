fd2b2b3e80920df19b1c5f882e451e7bde4dbdcc
James Best <jim@justjibba.net>
2022-09-26 16:39:42 +0100

    Add the first interfaces for the ast

---
+++ monkey/ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import "github.com/jim-at-jibba/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+// statementNode() and expressionNode() are dummy helpers
+// to enable the Go compiler to help us when we mix the 2 interfaces
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
