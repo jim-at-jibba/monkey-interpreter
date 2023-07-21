8c9060b5594db439298be0d5d3cf109ff8bcc9cb
James Best <james.best@breedr.co>
2023-07-21 15:08:02 +0100

    Add first tokens and token type

---
+++ monkey/token/token.go
@@ -0,0 +1,36 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"   // 12343
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keyworkds
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// Using a string for ease of use and debugging.
+// Not as porformant but perfectly fine
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
