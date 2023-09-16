d7865e3006de9e920117cdbcf5edcb9df0a0cf9f
James Best <jim@justjibba.net>
2023-09-16 06:12:42 +0100

    Parser first steps

---
+++ monkey/parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"github.com/jim-at-jibba/monkey/ast"
+	"github.com/jim-at-jibba/monkey/lexer"
+	"github.com/jim-at-jibba/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read 2 tokens so that curToken nad peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
